599ff693ce31b5fb0953bbe725eb586579a7528e
Lalitha Ganesan <ltganesan05@gmail.com>
2013-10-01 15:42:46 -0700

    Option parsing
    Still need to debug this

--- downloader.rb
+++ downloader.rb
@@ -1,4 +1,32 @@
-require "open-uri"
+require 'open-uri' # for url parsing
+require 'optparse' # for command line aoption parsing
+
+
+# sub-reddits to download from
+sub-reddits = ['EarthPorn', 'WaterPorn','nature','wallpaper']
+dir = 'Wallpapers'
+
+options = {
+  :output_dir => dir,
+  :reddits => reddits
+}
+
+optparse = OptionParser.new do|opts|
+	opts.on('-d', '--dir DIR', String, 'Download directory') do |dir|
+    	options[:output_dir] = dir
+    end
+    opts.on('r', '--r a,b,c,d', String, 'Subreddits comma separated') do |subs|
+    	options[:reddits] = subs
+    end
+	opts.on( '-h', '--help', 'Display this screen' ) do
+    	puts opts
+    	exit
+    end
+end
+
+optparse.parse!
+
+
 
 
 def download_images(url)
@@ -7,4 +35,6 @@ def download_images(url)
     	file.puts f.read
    		end
    	}
+   	return true
 end
+
